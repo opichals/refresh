40082cdd740d1919fc98363a4656d6eeaa084c27
Standa Opichal <opichals@gmail.com>
2018-03-18 16:56:43 +0100

    Updated to node 6.12.x, fixed PROPFIND depth: 1

--- provider.js
+++ provider.js
@@ -1,43 +1,56 @@
-var data = [];
+var data = { 'test': { 'a.txt': { mime: 'text/plain', content: 'a.contents' } } };
 
 function find(path) {
-  if (path === '/' || path === '') {
-    return data;
-  }
-  return path.replace(/[^a-z\/]/g, '').split('/').reduce(function (data, name) {
+  return path.replace(/[^\._A-Za-z\/]/g, '').split('/').reduce(function (data, name) {
     if (name === "") return data;
+    // console.log('fnd:', data, name);
     return data[name];
   }, data);
 }
 
-module.exports = {
-  getProp: function (path, name) {
-    var obj = find(path);
+
+function getSingleProp(obj, name) {
     if (name === "getlastmodified") {
       return (new Date()).toUTCString();
     }
-    if (name === "resourcetype") {
-      return "container";
+    if (name === "creationdate") {
+      return (new Date(0)).toUTCString();
     }
-    if (obj.hasOwnProperty(name)) {
+    if (name === "resourcetype") {
+      return obj && typeof obj.content === 'string' ? "" : "<D:collection/>";
+    }
+    if (name === "getcontentlength") {
+      return obj && obj.content && obj.content.length || 0;
+    }
+    if (obj && obj.hasOwnProperty(name)) {
       return obj[name];
     }
+}
+
+module.exports = {
+  getProp: function (path, props) {
+    var obj = find(path);
+        if (obj && obj.content) {
+          console.log('obj.len', path, typeof obj.content, obj.content.length);
+        }
+    if (!obj) {
+      return undefined;
+    }
+    return props.reduce((ps, name) => {
+        ps[name] = getSingleProp(obj, name);
+        return ps;
+    }, {});
   },
   get: function (path) {
-    var pos = path.lastIndexOf('/');
-    var name = path.substr(pos + 1);
-    path = path.substr(0, pos);
-    var parent = find(path);
-    if (parent && parent.hasOwnProperty(name)) {
-      return parent[name];
-    }
+    return find(path);
   },
   put: function (path, contents) {
     var pos = path.lastIndexOf('/');
     var name = path.substr(pos + 1);
     path = path.substr(0, pos);
     var parent = find(path);
-    if (parent && !parent.hasOwnProperty(name)) {
+    if (parent) {
+    // if (parent && !parent.hasOwnProperty(name)) {
       parent[name] = contents;
       return parent[name];
     }
--- test.js
+++ test.js
@@ -3,16 +3,7 @@ var source = require('./provider');
 var PORT = 6543;
 var HOST = "127.0.0.1";
 
-var tree = {
-  people: [
-    {name: "Tim", age: 27},
-    {name: "Jack", age: 3}
-  ],
-  colors: [0xff8800, 0x223322]
-}
-
 var http = require('http');
-process.mixin(require('sys'));
 
 // "OPTIONS, GET, HEAD, POST, TRACE, PROPFIND, PROPPATCH, MKCOL, COPY, PUT, DELETE, MOVE, LOCK, UNLOCK, BIND, REBIND, UNBIND, VERSION-CONTROL"
 var Handlers = {
@@ -21,69 +12,98 @@ var Handlers = {
       Dav: "1,2",
       Allow: Object.keys(Handlers).join(', '),
     });
-    res.close();
+    res.end();
   },
   PROPFIND: function (req, res, body) {
-    var results = {};
     var xml = Xml.parse(body);
-    Object.keys(xml.propfind.prop).forEach(function (name) {
-      var value = source.getProp(req.url, name);
-      var group = "HTTP/1.1 200 OK";
-      if (value === undefined) {
-        var group = "HTTP/1.1 404 Not Found";
-      }
-      if (!results[group]) {
-        results[group] = {};
-      }
-      results[group][name] = value;
-    })
-    var output = Xml.render({
-      multistatus: {
-        response: {
-          href: address + req.url,
-          propstat: Object.keys(results).map(function (group) {
-            return {
-              prop: results[group],
-              status: group
-            }
-          })
+    const depth = parseInt(req.headers['depth']);
+    //console.log('>PROPFIND', body, JSON.stringify(req.headers, null, 2));
+    xml.depth = depth;
+    xml.url = req.url;
+    // console.log('>PROPFIND', JSON.stringify(xml, null, 2));
+
+    function propfind(url, props) {
+        var value = source.getProp(url, props);
+
+        var group = "HTTP/1.1 200 OK";
+        if (value === undefined) {
+          var group = "HTTP/1.1 404 Not Found";
         }
-      }
-    });
-    puts(output);
+
+        var results = {};
+        if (!results[group]) {
+          results[group] = {};
+        }
+        results[group] = value;
+
+        // console.log('>propfind', url, results);
+        return {
+            response: {
+              href: url,
+              propstat: Object.keys(results).map(function (group) {
+                return { prop: results[group], status: group };
+              })
+            }
+        };
+    }
+    var propnames = Object.keys(xml.propfind.prop);
+    var v = propfind(req.url, propnames);
+    if (v.response.propstat && v.response.propstat[0] && v.response.propstat[0].status === 'HTTP/1.1 404 Not Found') {
+      res.writeHeader(404, "Not Found", {});
+      res.end();
+      return;
+    };
+    if (depth > 0) {
+        const o = source.get(req.url);
+        var list = Object.keys(o).map(item => propfind(req.url+''+item, propnames));
+        list.unshift(v);
+        v = list;
+    }
+
+    var output = Xml.render({ multistatus: { response: v } });
+    // console.log('<getProp', JSON.stringify(v, null, 2));
     res.writeHeader(207, "Multi Status", {
       "Content-Type": 'text/xml; charset="utf-8"',
       "Content-length": output.length
     });
     res.write(output);
-    res.close();
+    res.end();
   },
   MKCOL: function (req, res, body) {
     if (source.makeCol(req.url)) {
       res.writeHeader(201, "Created", {});
-      res.close();
+      res.end();
     } else {
       res.writeHeader(409, "Conflict", {});
-      res.close();
+      res.end();
     }
   },
   DELETE: function (req, res, body) {
     // TODO: Implement
     if (source.del(req.url)) {
       res.writeHeader(204, "No Content", {});
-      res.close();
+      res.end();
     } else {
       res.writeHeader(404, "Not Found", {});
-      res.close();
+      res.end();
     }
   },
+  LOCK: function (req, res, body) {
+    res.writeHeader(200, "OK", {});
+    res.end();
+  },
+  UNLOCK: function (req, res, body) {
+    res.writeHeader(200, "OK", {});
+    res.end();
+  },
   PUT: function (req, res, body) {
+    console.log("PUT: ", req.headers['content-type'], body && body.length, JSON.stringify(req.headers, null, 2));
     if (source.put(req.url, { mime: req.headers['content-type'], content: body})) {
       res.writeHeader(201, "Created", {});
-      res.close();
+      res.end();
     } else {
       res.writeHeader(409, "Conflict", {});
-      res.close();
+      res.end();
     }
   },
   GET: function (req, res, body) {
@@ -94,56 +114,61 @@ var Handlers = {
         "Content-Length": data.content.length
       });
       res.write(data.content);
-      res.close();
+      res.end();
     } else {
       res.writeHeader(404, {});
-      res.close();
+      res.end();
     }
   },
   POST: function () {},
 };
 
 http.createServer(function (req, res) {
-  puts("\"" + req.method + " " + req.url + " HTTP/" + req.httpVersionMajor + "." + req.httpVersionMinor + "\"");
-  p(source.data);
-  var close = res.close;
-  res.close = function () {
+  console.log(req.method + " " + req.url + " HTTP/" + req.httpVersionMajor + "." + req.httpVersionMinor);
+  const p = console.log;
+  // p(source.data);
+
+  var end = res.end;
+  res.end = function () {
     // Common Log Format (mostly)
-    puts(req.connection.remoteAddress + " - - [" + (new Date()).toUTCString() + "] \"" + req.method + " " + req.url + " HTTP/" + req.httpVersionMajor + "." + req.httpVersionMinor + "\" " + res.statusCode + " " + res.output[0].length + " \"" + (req.headers['referrer'] || "") + "\" \"" + req.headers["user-agent"] + "\"");
-    return close.apply(res, arguments);
+    p(/*req.connection.remoteAddress + " - - [" + (new Date()).toUTCString() + "] \"" + */ req.method + " " + req.url + " HTTP/" + req.httpVersionMajor + "." + req.httpVersionMinor + "\" " + res.statusCode + " " + (res.output[0] && res.output[0].length) + " \"" + (req.headers['referrer'] || "") + "\""); // \"" + req.headers["user-agent"] + "\"");
+    return end.apply(res, arguments);
   }
+
   var writeHeader = res.writeHeader;
   res.writeHeader = function (code) {
     res.statusCode = code;
     return writeHeader.apply(res, arguments);
   }
+
   function get_body(callback) {
     var content = '';
     req.addListener('data', function (chunk) {
-      content += chunk;
+      content += chunk.toString('binary');
     });
     req.addListener('end', function () {
       callback(content);
     });
   }
+
   if (Handlers[req.method]) {
     get_body(function (body) {
       Handlers[req.method](req, res, body);
     });
   } else {
     get_body(function (body) {
-      p(req);
+      // p(req);
       p(body);
       res.writeHeader(500, {'Content-Type': 'text/plain'});
       res.write(body);
-      res.close();
+      res.end();
     });
   }
 }).listen(6543);
 
 var address = "http://" + HOST + ":" + PORT;
-puts("SERVER STARTED ON " + address);
+console.log("SERVER STARTED ON " + address);
 
 process.addListener("uncaughtException", function (err) {
-  error(err.stack);
+  console.error(err.stack);
 });
